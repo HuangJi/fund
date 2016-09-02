4e473e2a9d4ca3484510fca6fe672dbe26b63b2b
Ubuntu <ubuntu@ip-172-31-19-90.ap-northeast-1.compute.internal>
2016-09-02 06:20:39 +0000

    Add Authorization into Access-Control-Allow-Headers

--- howhoworg/app.js
+++ howhoworg/app.js
@@ -74,9 +74,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
 });
 
 app.use('/', routes);
