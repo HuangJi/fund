e71f539dd6bce5ff1233d640c1b9d3eb58f28e5a
HuangJi <a1345962293@gmail.com>
2016-09-05 13:35:01 +0800

    Deleted app.js semicon.

--- howhoworg/app.js
+++ howhoworg/app.js
@@ -1,97 +1,98 @@
-const express = require('express');
-const path = require('path');
-// const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const http = require('http');
-const debug = require('debug')('monitor:server');
+const express = require('express')
+const path = require('path')
+// const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const http = require('http')
+const debug = require('debug')('monitor:server')
 
-const routes = require('./routes/index');
-const users = require('./routes/users');
-const api = require('./routes/api');
+const routes = require('./routes/index')
+const users = require('./routes/users')
+const api = require('./routes/api')
 
-const app = express();
+const app = express()
 
 function normalizePort(val) {
-  const nPort = parseInt(val, 10);
+  const nPort = parseInt(val, 10)
   if (isNaN(nPort)) {
-    return val;
+    return val
   }
   if (nPort >= 0) {
-    return nPort;
+    return nPort
   }
-  return false;
+  return false
 }
 
-const port = normalizePort(process.env.PORT || '1337');
+const port = normalizePort(process.env.PORT || '1337')
 
 function onError(error) {
   if (error.syscall !== 'listen') {
-    throw error;
+    throw error
   }
-  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
+  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.log(`${bind} requires elevated privileges`);
-      process.exit(1);
-      break;
+      console.log(`${bind} requires elevated privileges`)
+      process.exit(1)
+      break
     case 'EADDRINUSE':
-      console.log(`${bind} is already in use`);
-      process.exit(1);
-      break;
+      console.log(`${bind} is already in use`)
+      process.exit(1)
+      break
     default:
-      throw error;
+      throw error
   }
 }
 
-const server = http.createServer(app);
-server.listen(port);
-server.on('error', onError);
+const server = http.createServer(app)
+server.listen(port)
+server.on('error', onError)
 
 function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
-  debug(`Listening on ${bind}`);
-  console.log(`Server is running on port: ${addr.port}`);
+  const addr = server.address()
+  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`
+  debug(`Listening on ${bind}`)
+  console.log(`Server is running on port: ${addr.port}`)
 }
 
-server.on('listening', onListening);
+server.on('listening', onListening)
 
 
-app.set('port', port);
+app.set('port', port)
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 // uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers',
+              'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  next()
+})
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/api', api);
+app.use('/', routes)
+app.use('/users', users)
+app.use('/api', api)
 
 // catch 404 and forward to error handler
 app.use((req, res) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  res.status(404);
-  res.render('page404', {});
-  // next(err);
-});
+  const err = new Error('Not Found')
+  err.status = 404
+  res.status(404)
+  res.render('page404', {})
+  // next(err)
+})
 
 // error handlers
 
@@ -99,22 +100,22 @@ app.use((req, res) => {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use((err, req, res) => {
-    res.status(err.status || 500);
+    res.status(err.status || 500)
     res.render('error', {
       message: err.message,
       error: err,
-    });
-  });
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use((err, req, res) => {
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {},
-  });
-});
+  })
+})
 
-module.exports = app;
+module.exports = app
